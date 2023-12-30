df7a6a9366661ab7339882c9b76e862f63450d11
Matt Ehrnschwender <matthewe2020@gmail.com>
2023-12-29 21:12:18 -0500

    Move back to rlib for thanatos core

--- Payload_Type/thanatos/agent/thanatos_core/src/lib.rs
+++ Payload_Type/thanatos/agent/thanatos_core/src/lib.rs
@@ -1,9 +1,8 @@
 //! Core Thanatos agent
 
+use utils::hexdump::{self, HexdumpFormat};
+
 /// Main entrypoint for the agent
-#[no_mangle]
-pub extern "C" fn entrypoint(config_data: *const u8, config_data_len: usize) {
-    let config_data = unsafe { std::slice::from_raw_parts(config_data, config_data_len) };
-    let config: config::ConfigVars<'_> = rmp_serde::from_slice(config_data).unwrap();
-    dbg!(&config);
+pub fn entrypoint(config_data: &[u8]) {
+    hexdump::hexdump(config_data, HexdumpFormat::XxdColored);
 }
