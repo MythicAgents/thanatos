cde8edcb1c35cfc383d3acf9dd3ed73304691f96
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-02-25 01:04:52 +0000

    Pls work

---
+++ .github/scripts/sanitizers.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+REPO_BASE=""
+AGENT_CODE="Payload_Type/thanatos/agent"
+
+# Populates the 'REPO_BASE' to the base of the repo
+repo_base() {
+    # Get the path to the directory containing this script
+    local _script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+    # Traverse up to the base of the git repository
+    local _repo_base_dir=${_script_dir}/../..
+
+    # Ensure that the repo base contains the '.git' directory
+    if [ ! -d "${_repo_base_dir}/.git" ]; then
+        echo "Could not find git repository base"
+        exit 1
+    fi
+
+    # Set the REPO_BASE variable to the base of the repo
+    REPO_BASE="$(realpath ${_repo_base_dir})"
+}
+
+sanitizers() {
+    pushd $AGENT_CODE &> /dev/null
+    local _cmd="cargo build -p genconfig"
+    echo "current directory: $PWD"
+    echo "command: $_cmd"
+    eval $_cmd
+
+    local _cmd="RUSTFLAGS='-Zsanitizer=address' cargo +nightly test -Zbuild-std --color always -p ffiwrappers --all-features --target x86_64-unknown-linux-gnu"
+    echo "current directory: $PWD"
+    echo "command: $_cmd"
+    eval $_cmd
+
+    local _cmd="RUSTFLAGS='-Zsanitizer=memory' cargo +nightly test -Zbuild-std --color always -p ffiwrappers --all-features --target x86_64-unknown-linux-gnu"
+    echo "current directory: $PWD"
+    echo "command: $_cmd"
+    eval $_cmd
+
+    local _cmd="RUSTFLAGS='-Zsanitizer=leak' cargo +nightly test -Zbuild-std --color always -p ffiwrappers --all-features --target x86_64-unknown-linux-gnu"
+    echo "current directory: $PWD"
+    echo "command: $_cmd"
+    eval $_cmd
+    popd &> /dev/null
+}
+
+set -e
+repo_base
+pushd $REPO_BASE &> /dev/null
+sanitizers
+popd &> /dev/null
